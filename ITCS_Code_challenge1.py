ff82d0f1d9bc61f66c02480f21a4dd743517be4d
GabrielleLaguerta11 <gabriellelaguerta11@gmail.com>
2026-08-15 18:42:38 +0800

    Delete ITCS_Code_challenge1.py
    I will just fix it po, kulang po pala asterisks ko hehe. sorry po

--- ITCS_Code_challenge1.py
+++
@@ -1,4 +0,0 @@
-
-diamond = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*         Hi, my name is   \t*\n\t\t\t\t\t\t\t\t\t\t*             Gabrielle R. Laguerta   \t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-
-print(diamond)
