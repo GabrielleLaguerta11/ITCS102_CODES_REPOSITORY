6701120efcd787af1d46471dec89a39b9e63d867
GabrielleLaguerta11 <gabriellelaguerta11@gmail.com>
2026-08-15 19:09:52 +0800

    Delete ITCS_Code_challenge1.py

--- ITCS_Code_challenge1.py
+++
@@ -1,4 +0,0 @@
-
-diamond = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t  Hi, my name is   \t\t*\n\n\t\t\t\t\t\t\t\t\t*\t\t      \"GABRIELLE R. LAGUERTA\"   \t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
-
-print(diamond)
