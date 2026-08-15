a12806be61765b48dcdd3840a8c911e564a62474
GabrielleLaguerta11 <gabriellelaguerta11@gmail.com>
2026-08-15 19:16:05 +0800

    Add files via upload

---
+++ ITCS_Code_challenge1.py
@@ -0,0 +1,4 @@
+
+diamond = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t  Hi! my name is   \t\t*\n\n\t\t\t\t\t\t\t\t\t*\t\t     \\\"GABRIELLE R. LAGUERTA\"\\ \t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+print(diamond)
