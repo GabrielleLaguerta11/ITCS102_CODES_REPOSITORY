a5d9db1a1ca3279e72358cd5abb656259aa2f99b
Gabrielle Laguerta <gabriellelaguerta11@gmail.com>
2026-09-07 18:14:17 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,12 @@
+hobbies = " "
+
+h = input("What are your hobbies? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + ", "
+
+h = input("What else? --> ")
+hobbies += h + ", "
+
+print("My hobbies are", hobbies)
